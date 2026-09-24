5894f814145bb9b56fca521addec5ed3dece7ead
mintra34673-crypto <mintra.34673@gmail.com>
2026-09-24 07:15:00 +0700

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import vue from '@vitejs/plugin-vue' // หรือ react ตามที่ใช้
 
 export default defineConfig({
+  plugins: [vue()],
   base: '/adminhome/',
-  plugins: [react()],
 })
